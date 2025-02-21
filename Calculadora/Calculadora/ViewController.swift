f9c8c299e962715e741feb0386f63709a59a4136
Ana Ganfornina <anies.llamani@gmail.com>
2025-02-21 20:32:06 +0100

    feat:cabiar displey según numero pulsado

--- Calculadora/Calculadora/ViewController.swift
+++ Calculadora/Calculadora/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var displayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func digitTouched(_ sender: UIButton) {
+        let digitText = sender.titleLabel?.text ?? ""
+        displayLabel.text  = digitText
+    }
+    
+    @IBAction func operatorTouched(_ sender: UIButton) {
+        let operatorText = sender.titleLabel?.text ?? ""
+        print(operatorText)
+    }
 }
 
