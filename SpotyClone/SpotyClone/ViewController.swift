1e0ad80b7ea48b8155664ce4e8c27e60da85ce98
Ana Ganfornina <anies.llamani@gmail.com>
2025-02-27 13:24:45 +0100

    Initial commit sopotyclone

---
+++ SpotyClone/SpotyClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotyClone
+//
+//  Created by Ana on 26/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
