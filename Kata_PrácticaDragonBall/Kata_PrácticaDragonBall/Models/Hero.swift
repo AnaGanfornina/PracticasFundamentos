7bd1272a0d102441703a51701a8c84fa48f22900
Ana Ganfornina <anies.llamani@gmail.com>
2025-03-09 19:10:09 +0100

    DragonBall, feat: Configurado APIClient

---
+++ Kata_PrácticaDragonBall/Kata_PrácticaDragonBall/Models/Hero.swift
@@ -0,0 +1,17 @@
+//
+//  Hero.swift
+//  Kata_PrácticaDragonBall
+//
+//  Created by Ana on 8/3/25.
+//
+
+import Foundation
+
+
+struct Hero {
+    let id: String
+    let name: String
+    let favorite: Bool
+    let photo: String
+    let description: String
+}
---
+++ Kata_PrácticaDragonBall/Kata_PrácticaDragonBall/Models/Transformation.swift
@@ -0,0 +1,16 @@
+//
+//  Transformation.swift
+//  Kata_PrácticaDragonBall
+//
+//  Created by Ana on 8/3/25.
+//
+
+import Foundation
+
+struct Transformation {
+    let name: String
+    let hero: Hero
+    let id: String
+    let photo: String
+    let description: String
+}
