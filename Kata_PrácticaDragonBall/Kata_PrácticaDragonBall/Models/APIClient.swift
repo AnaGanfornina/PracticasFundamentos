5e5924da93eabdf501aa92b1d67348d649c28098
Ana Ganfornina <anies.llamani@gmail.com>
2025-03-09 21:45:13 +0100

    DragonBall, feat: Configurado APIClient

--- Kata_PrácticaDragonBall/Kata_PrácticaDragonBall/Models/APIClient.swift
+++ Kata_PrácticaDragonBall/Kata_PrácticaDragonBall/Models/APIClient.swift
@@ -22,6 +22,8 @@ struct APIClient {
         self.session = session
     }
     
+    // MARK: - Método jwt para obtener el token
+    
     /// Método por el cual vamos a obtener el JWT de la API a traves del URLRequest
     func jwt(
         _ request: URLRequest,
@@ -60,6 +62,53 @@ struct APIClient {
             }
             
             completion(.success(jwt))
+        }
+        
+        task.resume()
+    }
+    
+    // MARK: - Método request para obtener de forma asíncrona datos
+    
+    /// Método que obtiene de forma asíncrona datos
+    func request<T:Decodable>(
+        _ request: URLRequest,
+        using: T.Type,
+        completion: @escaping (Result<T, APIClientError>) -> Void
+    ){
+        // Obtiene de forma asíncrona los datos de la request
+        let task = session.dataTask(with: request) { data, response, error in
+            
+            guard error == nil else {
+                // Si hay un error lo envio al completion, que lo ejecutará
+                if let error = error as? NSError {
+                    completion(.failure(.statusCode(code: error.code)))
+                } else {
+                    completion(.failure(.unknown))
+                }
+                return
+            }
+            
+            guard let data else {
+                completion(.failure(.noData))
+                return
+            }
+            
+            
+            let response = response as? HTTPURLResponse
+            
+            guard let response, response.statusCode == 200 else {
+                completion(.failure(.statusCode(code: response?.statusCode)))
+                return
+            }
+            
+            guard let decodeModel = try?JSONDecoder().decode(using, from: data) else {
+                completion(.failure(.decodingFailed))
+                return
+            }
+            
+            completion(.success(decodeModel))
+            
+            
         }
         
         task.resume()
